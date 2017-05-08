aed5560731f7c3d09b4041460e2b4d588a2aa8ff
kennCK <kennettecanales@gmail.com>
2017-05-08 17:50:12 +0800

    Header

---
+++ src/components/common_module/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+<script>
+export default {
+  name: 'header',
+  mounted(){
+  },
+  data(){
+    return {
+      hm: 'GoDigit',
+      fm: '@Copyright2017'
+    }
+  }
+}
+</script>
+
