dcc591b7c2e7661012eecfc772428882c9b39f2f
kennCK <kennettecanales@gmail.com>
2017-06-15 19:39:22 +0800

    Update Menu Settings

--- api/app/MenuSettings.php
+++ api/app/MenuSettings.php
@@ -7,22 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuSettings extends APIModel
 {
-  use SoftDeletes;
   protected $primaryKey = 'id';
   protected $table = 'menu_settings';
-  protected $fillable = array(
-      'parent_id',
-      'module',
-      'title',
-      'left_icon',
-      'right_icon',
-      'method',
-      'rank'
-  );
-  public $timestamps = true;
-  protected $dates = ['deleted_at'];
-  public function menu()
-  {
-    return $this->belongsTo('\App\MenuSettings');
-  }
 }
