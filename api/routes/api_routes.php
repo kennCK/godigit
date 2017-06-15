068bbc6dde616c84d75d3612416801585f1fbe2b
kennCK <kennettecanales@gmail.com>
2017-06-15 13:56:24 +0800

    Update Frame
    Frames update

--- api/routes/api_routes.php
+++ api/routes/api_routes.php
@@ -1,9 +1,6 @@
 <?php
 /* Add the undescore case of the controller, no need to specify crud operations*/
 $apiResources = [
-  'menu_settings',
-  'department',
-  'department_members',
   'position',
   'position_history'
 ];
