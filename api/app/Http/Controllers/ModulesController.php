998df67eda2beeca33b1b1954be90a2627787ca0
kennCK <kennettecanales@gmail.com>
2017-06-16 14:30:06 +0800

    Update ModulesController.php

--- api/app/Http/Controllers/ModulesController.php
+++ api/app/Http/Controllers/ModulesController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\MenuSettings;
+use App\Modules;
 use DB;
 
 class ModulesController extends APIController
 {
   function __construct(){
-    $this->model = new MenuSettings();
+    $this->model = new Modules();
   }
 }
