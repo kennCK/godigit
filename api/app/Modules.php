65c1365397ac9ee286b1883af257e67d5d544715
kennCK <kennettecanales@gmail.com>
2017-06-16 14:23:57 +0800

    Update Modules.php

--- api/app/Modules.php
+++ api/app/Modules.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class MenuSettings extends APIModel
+class Modules extends APIModel
 {
   protected $primaryKey = 'id';
   protected $table = 'menu_settings';
